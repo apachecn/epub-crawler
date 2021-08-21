9f0f9792081e32fd0d88c0ef66c80c5823f449e3
wizardforcel <562826179@qq.com>
2021-08-21 12:46:31 +0800

    2021-08-21 12:46:31

--- EpubCrawler/__init__.py
+++ EpubCrawler/__init__.py
@@ -4,7 +4,10 @@
 """EpubCrawler
 https://github.com/apachecn/epub-crawler"""
 
+from . import img
+from . import util
+
 __author__ = "ApacheCN"
 __email__ = "apachecn@163.com"
 __license__ = "SATA"
-__version__ = "2021.8.20.0"
+__version__ = "2021.8.21.0"
--- EpubCrawler/util.py
+++ EpubCrawler/util.py
@@ -3,6 +3,24 @@
 import requests
 from imgyaso import pngquant_bts, \
     adathres_bts, grid_bts, noise_bts, trunc_bts
+import os
+import shutil
+
+is_pic = lambda x: x.endswith('.jpg') or \
+                   x.endswith('.jpeg') or \
+                   x.endswith('.png') or \
+                   x.endswith('.gif') or \
+                   x.endswith('.tiff') or \
+                   x.endswith('.bmp') or \
+                   x.endswith('.webp')
+
+def safe_mkdir(dir):
+    try: os.mkdir(dir)
+    except: pass
+    
+def safe_rmdir(dir):
+    try: shutil.rmtree(dir)
+    except: pass
 
 def request_retry(method, url, retry=10, **kw):
     kw.setdefault('timeout', 10)
